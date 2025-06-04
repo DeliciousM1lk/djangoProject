17a53ef1c2c03517710476021fe87be41a2159e2
DeliciousM1lk <vladdeev00@gmail.com>
2025-06-04 21:18:15 +0500

    1

--- core/views.py
+++ core/views.py
@@ -1,3 +1,8 @@
-from django.shortcuts import render
+from cmath import acosh
 
-# Create your views here.
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.shortcuts import render
+from django.views.generic import TemplateView
+
+class HomeView(LoginRequiredMixin, TemplateView):
+    template_name = 'registration/home.html'
