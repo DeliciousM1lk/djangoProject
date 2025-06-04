2f3ba24458501f2beb5ff106832bae2cfdcb2d37
DeliciousM1lk <vladdeev00@gmail.com>
2025-06-04 20:13:16 +0500

    12

--- project/settings.py
+++ project/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'app.apps.AppConfig',
     'films.apps.FilmsConfig',
     'bootstrap5',
+    'core.apps.CoreConfig',
 ]
 
 MIDDLEWARE = [
@@ -127,3 +128,8 @@ STATICFILES_DIRS = [
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+LOGIN_URL = 'login'
+LOGIN_REDIRECT_URL = 'home'
+LOGOUT_REDIRECT_URL = 'login'
