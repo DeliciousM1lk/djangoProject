5856dbc31d7317ac710f23568df18b57498687fc
DeliciousM1lk <vladdeev00@gmail.com>
2025-06-09 20:51:23 +0500

    1

--- core/urls.py
+++ core/urls.py
@@ -3,4 +3,6 @@ from .views import *
 
 urlpatterns = [
     path('', HomeView.as_view(), name='home'),
+    path('/posts/', PostListView.as_view(), name='post_list'),
+    path('posts/new/', PostCreateView.as_view(), name='post_create'),
 ]
--- core/views.py
+++ core/views.py
@@ -1,8 +1,25 @@
-from cmath import acosh
-
-from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.auth.mixins import LoginRequiredMixin, PermissionRequiredMixin
 from django.shortcuts import render
-from django.views.generic import TemplateView
+from django.views.generic import TemplateView, CreateView, ListView
+from .models import Post
 
 class HomeView(LoginRequiredMixin, TemplateView):
     template_name = 'registration/home.html'
+
+
+class PostListView(PermissionRequiredMixin, ListView):
+    permission_required = "core.view_secret_post"
+    model = Post
+    template_name = "registration/post_list.html"
+
+
+class PostCreateView(PermissionRequiredMixin, CreateView):
+    permission_required = "core.view_secret_post"
+    model = Post
+    template_name = "registration/post_create.html"
+    fields = ['title', 'body']
+    success_url = "core/posts/"
+
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
