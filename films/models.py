13bc061380020f622fb05ac3b11f5641456bdbda
DeliciousM1lk <vladdeev00@gmail.com>
2025-06-23 20:09:45 +0500

    1

--- films/models.py
+++ films/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 
+
 class Actor(models.Model):
     name = models.CharField(max_length=100)
 
@@ -7,13 +8,38 @@ class Actor(models.Model):
         return self.name
 
 
-class Film(models.Model):
+class BaseContent(models.Model):
     slug = models.SlugField(unique=True)
     name = models.CharField(max_length=100)
-    author = models.CharField(max_length=100)
     genre = models.CharField(max_length=100)
-    year = models.DateField()
-    actors = models.ManyToManyField(Actor, related_name="films")
+
+    class Meta:
+        abstract = True
 
     def __str__(self):
         return self.name
+
+
+class MediaContent(BaseContent):
+    author = models.CharField(max_length=100)
+    year = models.DateField()
+
+    class Meta:
+        abstract = True
+
+
+class Film(MediaContent):
+    actors = models.ManyToManyField(Actor, related_name="films")
+    duration_minutes = models.PositiveIntegerField()
+
+    def get_type(self):
+        return "Фильм"
+
+
+class Series(MediaContent):
+    episodes = models.PositiveIntegerField()
+    seasons = models.PositiveIntegerField()
+    actors = models.ManyToManyField(Actor, related_name="series")
+
+    def get_type(self):
+        return "Сериал"
