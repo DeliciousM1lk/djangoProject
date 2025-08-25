79e49e10f38bee79f5a7d527c1ccbeeb65c4f5c1
DeliciousM1lk <vladdeev00@gmail.com>
2025-08-25 20:38:37 +0500

    1

--- films/models.py
+++ films/models.py
@@ -1,14 +1,34 @@
 from django.db import models
+from django.contrib.postgres.fields import ArrayField, HStoreField
+from django.contrib.postgres.fields.citext import CICharField
+
 
 class Film(models.Model):
-    slug=models.SlugField(unique=True,blank=False,null=False)
-    name=models.CharField(max_length=100,null=False,blank=False)
-    author=models.CharField(max_length=100)
-    genre=models.CharField(max_length=100)
-    year=models.DateField(null=False,blank=False)
-    # file=models.FileField(upload_to='films/', null=False, blank=False)
+    slug = models.SlugField(unique=True, blank=False, null=False)
+    name = models.CharField(max_length=100, null=False, blank=False)
+    author = models.CharField(max_length=100)
+    genre = models.CharField(max_length=100)
+    year = models.DateField(null=False, blank=False)
+
+    tags = ArrayField(
+        models.CharField(max_length=50),
+        size=5,
+        blank=True,
+        default=list,
+        help_text="Список тегов к фильму"
+    )
+
+    metadata = HStoreField(
+        blank=True,
+        null=True,
+        help_text="Произвольные пары ключ=значение"
+    )
+
+    ci_title = CICharField(
+        max_length=100,
+        unique=True,
+        help_text="Название без учёта регистра"
+    )
 
     def __str__(self):
         return self.name
-
-
